b2585b9c06d0ee8210197c886969bff369fadf69
Crauzer <filip.quitko@gmail.com>
2017-09-01 21:22:54 +0200

    Initial Project

---
+++ Prism/Enums.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Prism
+{
+    [Flags]
+    public enum ThreadContextFlags : uint
+    {
+        i386 = 0x10000,
+        i486 = 0x10000,
+        Control = i386 | 0x01,
+        Integer = i386 | 0x02,
+        Segments = i386 | 0x04,
+        FloatingPoint = i386 | 0x08,
+        DebugRegisters = i386 | 0x10,
+        ExtendedRegisters = i386 | 0x20,
+        Full = Control | Integer | Segments,
+        All = Control | Integer | Segments | FloatingPoint | DebugRegisters | ExtendedRegisters
+    }
+
+    [Flags]
+    public enum ThreadAccess : uint
+    {
+        Terminate = 1 << 0,
+        SuspendResume = 1 << 1,
+        GetContext = 1 << 3,
+        SetContext = 1 << 4,
+        SetInformation = 1 << 5,
+        QueryInformation = 1 << 6,
+        SetThreadToken = 1 << 7,
+        Impersonate = 1 << 8,
+        DirectImpersonation = 1 << 9,
+        SetLimitedInformation = 1 << 10,
+        QueryLimitedInformation = 1 << 11,
+        Synchronize = 1 << 20,
+        All = 0x1F03FF
+    }
+
+    public enum MemoryAllocation
+    {
+        Commit = 0x1000,
+        Reserve = 0x2000,
+        Decommit = 0x4000,
+        Release = 0x8000,
+        Reset = 0x80000,
+        Physical = 0x400000,
+        TopDown = 0x100000,
+        WriteWatch = 0x200000,
+        LargePages = 0x20000000
+    }
+
+    [Flags]
+    public enum Protection : uint
+    {
+        NoAccess = 1 << 0,
+        ReadOnly = 1 << 1,
+        ReadWrite = 1 << 2,
+        WriteCopy = 1 << 3,
+        Execute = 1 << 4,
+        ExecuteRead = 1 << 5,
+        ExecuteReadWrite = 1 << 6,
+        ExecuteWriteCopy = 1 << 7,
+        Guard = 1 << 8,
+        NoCache = 1 << 9,
+        WriteCombine = 1 << 10
+    }
+
+    public enum ProcessAccessRights : int
+    {
+        PROCESS_ALL_ACCESS = 0x1F0FFF,
+        PROCESS_CREATE_PROCESS = 0x0080,
+        PROCESS_CREATE_THREAD = 0x0002,
+        PROCESS_DUP_HANDLE = 0x0040,
+        PROCESS_QUERY_INFORMATION = 0x0400,
+        PROCESS_QUERY_LIMITED_INFORMATION = 0x1000,
+        PROCESS_SET_INFORMATION = 0x0200,
+        PROCESS_SET_QUOTA = 0x0100,
+        PROCESS_SUSPEND_RESUME = 0x0800,
+        PROCESS_TERMINATE = 0x0001,
+        PROCESS_VM_OPERATION = 0x0008,
+        PROCESS_VM_READ = 0x0010,
+        PROCESS_VM_WRITE = 0x0020,
+        SYNCHRONIZE = 0x00100000
+    }
+}
---
+++ Prism/MemoryEditor.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Prism
+{
+    public class MemoryEditor : IDisposable
+    {
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr OpenProcess(ProcessAccessRights dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+        [DllImport("kernel32.dll")]
+        private static extern bool ReadProcessMemory(int hProcess, int lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
+        [DllImport("kernel32.dll")]
+        private static extern bool WriteProcessMemory(int hProcess, int lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesWritten);
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr OpenThread(ThreadAccess dwDesiredAccess, bool bInheritHandle, uint dwThreadId);
+        [DllImport("kernel32.dll")]
+        private static extern uint SuspendThread(IntPtr hThread);
+        [DllImport("kernel32.dll")]
+        private static extern int ResumeThread(IntPtr hThread);
+        [DllImport("Kernel32")]
+        private static extern bool CloseHandle(IntPtr handle);
+        [DllImport("kernel32.dll")]
+        public static extern bool GetThreadContext(IntPtr hThread, ref ThreadContext lpContext);
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, int dwSize, MemoryAllocation flAllocationType, Protection flProtect);
+        [DllImport("kernel32.dll")]
+        private static extern bool VirtualFree(IntPtr lpAddress, IntPtr dwSize, MemoryAllocation freeType);
+        [DllImport("kernel32.dll")]
+        private static extern bool VirtualProtectEx(IntPtr hProcess, uint dwAddress, int nSize, uint flNewProtect, out uint lpflOldProtect);
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, out uint lpThreadId);
+
+        public IntPtr Handle { get; private set; }
+        public IntPtr MainModuleAddress { get; private set; }
+        public int MainModuleSize { get; private set; }
+        public Process Process { get; private set; }
+        public bool IsActive { get; private set; }
+
+        public MemoryEditor(string processName)
+        {
+            this.IsActive = OpenProcess(processName);
+        }
+
+        public MemoryEditor(string processName, bool inheritHandle)
+        {
+            this.IsActive = OpenProcess(processName, inheritHandle);
+        }
+
+        public MemoryEditor(string processName, bool inheritHandle, ProcessAccessRights access)
+        {
+            this.IsActive = OpenProcess(processName, inheritHandle, access);
+        }
+
+        public bool OpenProcess(string processName)
+        {
+            return OpenProcess(processName, false, ProcessAccessRights.PROCESS_ALL_ACCESS);
+        }
+
+        public bool OpenProcess(string processName, bool inheritHandle)
+        {
+            return OpenProcess(processName, inheritHandle, ProcessAccessRights.PROCESS_ALL_ACCESS);
+        }
+
+        public bool OpenProcess(string processName, bool inheritHandle, ProcessAccessRights access)
+        {
+            Process[] processes = Process.GetProcessesByName(processName);
+            if (processes.Length != 0)
+            {
+                this.Process = processes[0];
+
+                try
+                {
+                    this.Handle = OpenProcess(access, inheritHandle, this.Process.Id);
+                    this.MainModuleAddress = this.Process.MainModule.BaseAddress;
+                    this.MainModuleSize = this.Process.MainModule.ModuleMemorySize;
+                    this.IsActive = true;
+                    return true;
+                }
+                catch (Exception)
+                {
+                    throw new Exception("Opening process: " + processes[0].ProcessName + " failed");
+                }
+            }
+            else
+            {
+                throw new Exception("Specified process not found");
+            }
+        }
+
+        public byte ReadByte(IntPtr address)
+        {
+            return ReadBytes(address, sizeof(byte))[0];
+        }
+
+        public short ReadInt16(IntPtr address)
+        {
+            return BitConverter.ToInt16(ReadBytes(address, sizeof(short)), 0);
+        }
+
+        public ushort ReadUInt16(IntPtr address)
+        {
+            return BitConverter.ToUInt16(ReadBytes(address, sizeof(ushort)), 0);
+        }
+
+        public int ReadInt32(IntPtr address)
+        {
+            return BitConverter.ToInt32(ReadBytes(address, sizeof(int)), 0);
+        }
+
+        public uint ReadUInt32(IntPtr address)
+        {
+            return BitConverter.ToUInt32(ReadBytes(address, sizeof(uint)), 0);
+        }
+
+        public long ReadInt64(IntPtr address)
+        {
+            return BitConverter.ToInt64(ReadBytes(address, sizeof(long)), 0);
+        }
+
+        public ulong ReadUInt64(IntPtr address)
+        {
+            return BitConverter.ToUInt64(ReadBytes(address, sizeof(ulong)), 0);
+        }
+
+        public IntPtr ReadIntPtr(IntPtr address)
+        {
+            return (IntPtr)BitConverter.ToInt32(ReadBytes(address, sizeof(uint)), 0);
+        }
+
+        public double ReadDouble(IntPtr address)
+        {
+            return BitConverter.ToDouble(ReadBytes(address, sizeof(double)), 0);
+        }
+
+        public float ReadFloat(IntPtr address)
+        {
+            return BitConverter.ToSingle(ReadBytes(address, sizeof(float)), 0);
+        }
+
+        public T ReadStruct<T>(IntPtr address) where T : struct
+        {
+            T structure = new T();
+
+            int structSize = Marshal.SizeOf(structure);
+            byte[] structBuffer = ReadBytes(address, structSize);
+            IntPtr structPointer = Marshal.AllocHGlobal(structSize);
+
+            Marshal.Copy(structBuffer, 0, structPointer, structSize);
+
+            structure = (T)Marshal.PtrToStructure(structPointer, structure.GetType());
+            Marshal.FreeHGlobal(structPointer);
+
+            return structure;
+        }
+
+        public byte[] ReadBytes(IntPtr address, int size)
+        {
+            int byteCount = 0;
+            byte[] buffer = new byte[size];
+
+            bool success = ReadProcessMemory((int)Handle, (int)address, buffer, buffer.Length, ref byteCount);
+
+            return success ? buffer : buffer = null;
+        }
+
+        public bool WriteByte(byte value, IntPtr address)
+        {
+            return WriteBytes(BitConverter.GetBytes(value), address);
+        }
+
+        public bool WriteInt16(short value, IntPtr address)
+        {
+            return WriteBytes(BitConverter.GetBytes(value), address);
+        }
+
+        public bool WriteUInt16(ushort value, IntPtr address)
+        {
+            return WriteBytes(BitConverter.GetBytes(value), address);
+        }
+
+        public bool WriteInt32(int value, IntPtr address)
+        {
+            return WriteBytes(BitConverter.GetBytes(value), address);
+        }
+
+        public bool WriteUInt32(uint value, IntPtr address)
+        {
+            return WriteBytes(BitConverter.GetBytes(value), address);
+        }
+
+        public bool WriteInt64(long value, IntPtr address)
+        {
+            return WriteBytes(BitConverter.GetBytes(value), address);
+        }
+
+        public bool WriteUInt64(ulong value, IntPtr address)
+        {
+            return WriteBytes(BitConverter.GetBytes(value), address);
+        }
+
+        public bool WriteIntPtr(IntPtr value, IntPtr address)
+        {
+            return WriteInt32(value.ToInt32(), address);
+        }
+
+        public bool WriteDouble(double value, IntPtr address)
+        {
+            return WriteBytes(BitConverter.GetBytes(value), address);
+        }
+
+        public bool WriteFloat(float value, IntPtr address)
+        {
+            return WriteBytes(BitConverter.GetBytes(value), address);
+        }
+
+        public bool WriteStruct<T>(T value, IntPtr address) where T : struct
+        {
+            int structSize = Marshal.SizeOf(value);
+            byte[] structBuffer = new byte[structSize];
+
+            IntPtr structPointer = Marshal.AllocHGlobal(structSize);
+            Marshal.StructureToPtr(value, structPointer, true);
+            Marshal.Copy(structPointer, structBuffer, 0, structSize);
+            Marshal.FreeHGlobal(structPointer);
+
+            return WriteBytes(structBuffer, address);
+        }
+
+        public bool WriteBytes(byte[] buffer, IntPtr address)
+        {
+            int byteCount = 0;
+            return WriteProcessMemory((int)this.Handle, (int)address, buffer, buffer.Length, ref byteCount);
+        }
+
+        public void Dispose()
+        {
+            CloseHandle(this.Handle);
+            this.IsActive = false;
+            this.Process = null;
+            this.MainModuleAddress = IntPtr.Zero;
+            this.MainModuleSize = 0;
+        }
+    }
+}
