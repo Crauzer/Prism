32caee8a18f8ba46165d3e01ea0e9732ce3fc39d
Crauzer <filip.quitko@gmail.com>
2017-09-05 20:42:48 +0200

    PrismTests

---
+++ PrismTests/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism;
+
+namespace PrismTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (MemoryEditor me = new MemoryEditor("TestCA"))
+            {
+                me.WriteInt32(1, new IntPtr(0x00E143D4));
+            }
+        }
+    }
+}
